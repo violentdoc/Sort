3455ec14cfe9aeecef429793ebd0fc452805cdae
Misbah Samio <60313194+violentdoc@users.noreply.github.com>
2020-03-28 17:20:44 +0600

    s

--- sorting algos/Bubble/bubble_sort.cpp
+++ sorting algos/Bubble/bubble_sort.cpp
@@ -9,7 +9,7 @@ int main(){
 	for(int i=0;i<n;i++)
 		cin >> arr[i];
 	for(int i=0;i<n;i++){
-		for(int j=0;j<n+1-i;j++){
+		for(int j=0;j<n-1-i;j++){
 			if(arr[j]>arr[j+1])
 				swap(arr[j],arr[j+1]);
 		}
